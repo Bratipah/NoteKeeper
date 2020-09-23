695ef3a5bb5500a3aee34fb0f0a49a957e86847d
Bratipah <bratipahmh@gmail.com>
2020-09-24 00:45:54 +0300

    made main activity the only launcher

--- app/src/main/java/com/example/notekeeper/MainActivity.java
+++ app/src/main/java/com/example/notekeeper/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void initializeDisplayContent() {
-        final RecyclerView recyclerNotes = (RecyclerView) findViewById(R.id.list_notes);
+        final RecyclerView recyclerNotes = (RecyclerView) findViewById(R.id.list_items);
         final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
         recyclerNotes.setLayoutManager(notesLayoutManager);
 
